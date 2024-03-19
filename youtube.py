ee2a37364c7be810e1770c4699bad7142276e3cd
Rajarajeswaris24 <153385139+Rajarajeswaris24@users.noreply.github.com>
2024-03-19 10:03:24 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -6,31 +6,31 @@ import mysql.connector as sql
 import pandas as pd
 import isodate
 from datetime import datetime
+import plotly.express as px
+import plotly.graph_objects as go
 
 #youtube
-api_key = 'AIzaSyDYqmIc6uGky8SBSX2cK6QQ2LrXTWoBJnY'
+api_key = ' '
 api_service_name = "youtube"
 api_version = "v3"
 youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
 
-# sql connection
+#mysql connection
 mydb=sql.connect(host="localhost",user="root",password="root",database= "youtube_data",port = "3306")
 cursor=mydb.cursor(buffered=True)
 #mongodb connection
-mongo_url=pymongo.MongoClient( "mongodb://raj:guvi2024@ac-nr8vpka-shard-00-00.vrmcz0d.mongodb.net:27017,ac-nr8vpka-shard-00-01.vrmcz0d.mongodb.net:27017,ac-nr8vpka-shard-00-02.vrmcz0d.mongodb.net:27017/?ssl=true&replicaSet=atlas-oe657d-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster1")
+mongo_url=pymongo.MongoClient( "mongodb://raj:guvi20@ac-nr8vpka-shard-00-00.vrmcz0d.mongodb.net:27017,ac-nr8vpka-shard-00-01.vrmcz0d.mongodb.net:27017,ac-nr8vpka-shard-00-02.vrmcz0d.mongodb.net:27017/?ssl=true&replicaSet=atlas-oe657d-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster1")
 
-#streamlit page
 st.set_page_config(
-    page_title="Streamlit App",
+    page_title="YouTube Data Harvesting and Warehousing",
     page_icon="▶️",
     layout="wide",
     initial_sidebar_state="auto")
 
-
 st.title("YouTube Data Harvesting and Warehousing")
 user_input = st.text_input("Enter Channel Id:")
 
-#function channel_details
+#functio channel_details
 def channel_details(channel_id):
   request = youtube.channels().list(
         part="snippet,contentDetails,statistics",
@@ -52,7 +52,7 @@ def channel_details(channel_id):
           "View_count" : response['items'][0]['statistics']['viewCount']}
   return data
 
-#function video_ids
+#functio get_videoids
 def get_videos_ids(channel_id):
   video_ids=[]
   resp=youtube.channels().list(id=channel_id,part='contentDetails').execute()
@@ -71,7 +71,7 @@ def get_videos_ids(channel_id):
        break
   return video_ids
 
-#function video_info
+#function video_details
 def get_video_info(VideoIds):
   video_datas=[]
   for video_id in VideoIds:
@@ -97,14 +97,14 @@ def get_video_info(VideoIds):
         video_datas.append(data)
   return video_datas
 
-#function comments
+#function comment_details
 def get_comment_info(VideoIds):
   Comment_data=[] 
   try:
     for video_id in VideoIds:
     #   next_page_token=None
     #   while True:
-        request=youtube.commentThreads().list(part="snippet",videoId=video_id,maxResults=50) #,pageToken=next_page_token)
+        request=youtube.commentThreads().list(part="snippet",videoId=video_id,maxResults=3)#,pageToken=next_page_token)
         response=request.execute()
         datetime_str = response['items'][0]['snippet']['topLevelComment']['snippet']['publishedAt']
         try:
@@ -112,7 +112,7 @@ def get_comment_info(VideoIds):
         except ValueError:
             datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S.%fZ')
         for item in response['items']:
-            data=dict(Comment_Id=item['snippet']['topLevelComment']['id'],Video_Id=item['snippet']['topLevelComment']['snippet']['videoId'], 
+            data=dict(Comment_Id=item['snippet']['topLevelComment']['id'],Channel_Id=item['snippet']['channelId'],Video_Id=item['snippet']['topLevelComment']['snippet']['videoId'], 
                     Comment_Text=item['snippet']['topLevelComment']['snippet']['textDisplay'],Comment_Author=item['snippet']['topLevelComment']['snippet']['authorDisplayName'],
                     Comment_Published=datetime_obj)
             Comment_data.append(data)
@@ -125,14 +125,14 @@ def get_comment_info(VideoIds):
     pass
   return Comment_data
 
-#function transfer channel
+
+#function transfer to mongodb
 def transfer_channel():    
     db=mongo_url['YouTube_Data']
     col=db['channel']
     bj=channel_details(user_input)
     col.insert_one(bj)
 
-#function transfer videos
 def transfer_videos():
     db=mongo_url['YouTube_Data']
     col1=db['video']
@@ -140,87 +140,117 @@ def transfer_videos():
     video_details=get_video_info(v_id)
     col1.insert_many(video_details)
 
-#function transfer comments
+
 def transfer_comments():    
     db=mongo_url['YouTube_Data']
-    col2=db['comments']
+    col2=db['comment']
     v_id=get_videos_ids(user_input)
     comment_details=get_comment_info(v_id)
     col2.insert_many(comment_details)
 
-#function st.session_state
-def is_transfer_videos_done():
-    if 'transfer_videos_done' not in st.session_state:
-        st.session_state.transfer_videos_done = False
-    return st.session_state.transfer_videos_done
-
-def is_transfer_comments_done():
-    if 'transfer_comments_done' not in st.session_state:
-        st.session_state.transfer_comments_done = False
-    return st.session_state.transfer_comments_done
   
-#function migrate channel
-def migrate_channel():    
+#function table_exists
+def table_exists(cursor,Channel):
+    cursor.execute("SHOW TABLES LIKE %s", (Channel,))
+    return cursor.fetchone() is not None
+
+def table_exists(cursor,video):
+    cursor.execute("SHOW TABLES LIKE %s", (video,))
+    return cursor.fetchone() is not None
+
+def table_exists(cursor,comments):
+    cursor.execute("SHOW TABLES LIKE %s", (comments,))
+    return cursor.fetchone() is not None
+
+
+#function migrate to mysql
+def migrate_channel(user_input):    
     db=mongo_url['YouTube_Data']
     col=db['channel']
-    cursor.execute("DROP TABLE IF EXISTS Channel")
-    cursor.execute("""
-        CREATE TABLE Channel (
-            Channel_Id VARCHAR(255),Channel_name VARCHAR(255),Description TEXT,Published_at DATETIME,
-                   Playlist_id VARCHAR(255),Subscriber_count INT,video_count INT,View_count INT,PRIMARY KEY(Channel_Id))""")
-    for document in col.find({}, {"_id": 0}):
+    if not table_exists(cursor,'Channel'):
+        
         cursor.execute("""
-            INSERT INTO Channel (
-                Channel_Id,Channel_name,Description,Published_at,Playlist_id,Subscriber_count, video_count,
-                       View_count) VALUES 
-                       ( %s, %s, %s, %s, %s, %s, %s,%s)""",                 
-            (document["Channel_Id"],document["Channel_name"],document["Description"], document["Published_at"], 
-              document["Playlist_id"], document["Subscriber_count"],
-            document["video_count"], document["View_count"] ))
+            CREATE TABLE Channel (
+                Channel_Id VARCHAR(255),Channel_name VARCHAR(255),Description TEXT,Published_at DATETIME,
+                    Playlist_id VARCHAR(255),Subscriber_count INT,video_count INT,View_count INT,PRIMARY KEY(Channel_Id))""")
+    try:
+        for document in col.find({"Channel_Id":user_input}, {"_id": 0}):
+            cursor.execute("""
+                INSERT INTO Channel (
+                    Channel_Id,Channel_name,Description,Published_at,Playlist_id,Subscriber_count, video_count,
+                        View_count) VALUES 
+                        ( %s, %s, %s, %s, %s, %s, %s,%s)""",                 
+                (document.get("Channel_Id"),document.get("Channel_name"),document.get("Description"), document.get("Published_at"), 
+                document.get("Playlist_id"), document.get("Subscriber_count"),
+                document.get("video_count"), document.get("View_count") ))
         mydb.commit()
+        return True
+    except Exception as e:
+        print("Error occurred during migration:", e)
+        mydb.rollback() 
+        return False
 
-#function migrate video
-def migrate_video():
+def migrate_video(user_input):
     db=mongo_url['YouTube_Data']
     col=db['video']
-    cursor.execute("DROP TABLE IF EXISTS video")
-    cursor.execute("""
-        CREATE TABLE video (
-            Channel_Name VARCHAR(255),Channel_Id VARCHAR(255),Video_Id VARCHAR(255),Title TEXT,Tags TEXT,Thumbnail VARCHAR(255),
-            Description TEXT,Published_Date DATETIME,Duration TIME,Views INT,likes INT,Comments INT,Favorite_Count INT,
-            Caption_Status VARCHAR(255))""")
-    for document in col.find({}, {"_id": 0}):
+    if not table_exists(cursor,'video'):
         cursor.execute("""
-            INSERT INTO video (
-                Channel_Name, Channel_Id, Video_Id, Title, Tags, Thumbnail,Description, Published_Date, Duration, 
-                       Views, likes, Comments,Favorite_Count, Caption_Status) VALUES 
-                       ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",                 
-            (document["Channel_Name"], document["Channel_Id"], document["Video_Id"], document["Title"],
-            document["Tags"], document["Thumbnail"], document["Description"], document["Published_Date"],
-            document["Duration"], document["Views"], document["likes"], document["Comments"],
-            document["Favorite_Count"], document["Caption_Status"]
-        ))
+            CREATE TABLE video (
+                Channel_Name VARCHAR(255),Channel_Id VARCHAR(255),Video_Id VARCHAR(255),Title TEXT,Tags TEXT,Thumbnail VARCHAR(255),
+                Description TEXT,Published_Date DATETIME,Duration TIME,Views INT,likes INT,Comments INT,Favorite_Count INT,
+                Caption_Status VARCHAR(255))""")
+    try:
+        cursor.execute("SELECT COUNT(*) FROM video WHERE Channel_Id = %s", (user_input,))
+        count = cursor.fetchone()[0]
+        if count > 0:
+            return False
+        for document in col.find({"Channel_Id":user_input}, {"_id": 0}):
+            cursor.execute("""
+                INSERT INTO video (
+                    Channel_Name, Channel_Id, Video_Id, Title, Tags, Thumbnail,Description, Published_Date, Duration, 
+                        Views, likes, Comments,Favorite_Count, Caption_Status) VALUES 
+                        ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",                 
+                (document.get("Channel_Name"), document.get("Channel_Id"), document.get("Video_Id"), document.get("Title"),
+                document.get("Tags"), document.get("Thumbnail"), document.get("Description"), document.get("Published_Date"),
+                document.get("Duration"), document.get("Views"), document.get("likes"), document.get("Comments"),
+                document.get("Favorite_Count"), document.get("Caption_Status")
+            ))
         mydb.commit()
+        return True
+    except Exception as e:
+        print("Error occurred during migration:", e)
+        mydb.rollback() 
+        return False
 
-#function migrate comments
-def migrate_comments():
+def migrate_comments(user_input):
     db=mongo_url['YouTube_Data']  
-    col=db['comments']
-    cursor.execute("DROP TABLE IF EXISTS comments")
-    cursor.execute("""
-        CREATE TABLE comments (
-            Comment_Id VARCHAR(255),Video_Id VARCHAR(255),Comment_Text TEXT,Comment_Author VARCHAR(255), 
-                 Comment_Published DATETIME)""")
-    for document in col.find({}, {"_id": 0}):
+    col=db['comment']
+    if not table_exists(cursor,'comments'):
+    
         cursor.execute("""
-            INSERT INTO comments (
-                Comment_Id,Video_Id,Comment_Text,Comment_Author,Comment_Published) VALUES 
-                       ( %s, %s, %s, %s, %s)""",                 
-            (document["Comment_Id"],document["Video_Id"],document["Comment_Text"],document["Comment_Author"], 
-              document["Comment_Published"] ))
+            CREATE TABLE comments (
+                Comment_Id VARCHAR(255),Channel_Id VARCHAR(255),Video_Id VARCHAR(255),Comment_Text TEXT,Comment_Author VARCHAR(255), 
+                    Comment_Published DATETIME)""")
+    try:
+        cursor.execute("SELECT COUNT(*) FROM comments WHERE Channel_Id = %s", (user_input,))
+        count = cursor.fetchone()[0]
+        if count > 0:
+            return False
+        for document in col.find({"Channel_Id":user_input}, {"_id": 0}):
+            cursor.execute("""
+                INSERT INTO comments (
+                    Comment_Id,Channel_Id,Video_Id,Comment_Text,Comment_Author,Comment_Published) VALUES 
+                        ( %s,%s, %s, %s, %s, %s)""",                 
+                (document.get("Comment_Id"),document.get("Channel_Id"),document.get("Video_Id"),document.get("Comment_Text"),document.get("Comment_Author"), 
+                document.get("Comment_Published") ))
         mydb.commit()
+        return True
+    except Exception as e:
+        print("Error occurred during migration:", e)
+        mydb.rollback() 
+        return False
 
-#function queries 10
+#function queries
 def Query1(): 
       query1=""" SELECT Channel_name, Title FROM video"""
       cursor.execute(query1)
@@ -237,6 +267,23 @@ def Query2():
       c2=cursor.column_names
       df2=pd.DataFrame(r2,columns=c2)
       st.write(df2)
+      st.write("### :green[Number of videos in each channel :]")
+      fig = go.Figure(go.Bar(x=df2['Channel_Name'],y=df2['No_of_videos'] ))
+      fig.update_layout(
+        title="No. of videos per channel",
+        xaxis_title="Channel_Name",
+        yaxis_title="No. of videos",
+        yaxis=dict(range=[0, 350]),
+        margin=dict(l=100, r=20, t=30, b=20), 
+        height=400  
+        )
+      st.plotly_chart(fig, use_container_width=True)
+      
+      fig=px.pie(df2,names="Channel_Name",values="No_of_videos")
+      fig.update_layout(
+        title="No.of videos per channel" )
+      st.plotly_chart(fig)
+      
 
 def Query3():
       query3="""SELECT Channel_Name,Title,Views FROM video ORDER BY Views DESC LIMIT 10"""
@@ -245,6 +292,16 @@ def Query3():
       c3=cursor.column_names
       df3=pd.DataFrame(r3,columns=c3)
       st.write(df3)
+      st.write("### :green[ Top 10 most viewed videos :]")
+      fig = go.Figure(go.Bar(y=df3['Title'],x=df3['Views'],orientation='h' ))
+      fig.update_layout(
+        title="Views per Video",
+        xaxis_title="Views",
+        yaxis_title="Video Title",
+        margin=dict(l=100, r=20, t=30, b=20), 
+        height=400  
+        )
+      st.plotly_chart(fig, use_container_width=True)
 
 def Query4():
       query4="""SELECT Title AS Video_names ,Comments AS comment_count FROM video"""
@@ -278,6 +335,18 @@ def Query7():
       c7=cursor.column_names
       df7=pd.DataFrame(r7,columns=c7)
       st.write(df7)
+      st.write("### :green[View count for each channel :]")
+      fig = go.Figure(go.Bar(y=df7['Channel_Name'],x=df7['View_count'],orientation='h' ))
+      fig.update_layout(
+        title="Views per Channel",
+        xaxis_title="Views",
+        yaxis_title="Channel Name",
+        xaxis=dict(range=[0, 300000000]),
+        margin=dict(l=100, r=20, t=30, b=20), 
+        height=400  
+        )
+      st.plotly_chart(fig, use_container_width=True)
+      
 
 def Query8():
       query8="""SELECT Channel_Name, count(Channel_Name) AS Total_videos FROM video WHERE YEAR(Published_Date)=2022 GROUP by Channel_Name"""
@@ -286,6 +355,11 @@ def Query8():
       c8=cursor.column_names
       df8=pd.DataFrame(r8,columns=c8)
       st.write(df8)
+      st.write("### :green[No. of videos published in 2022 for each channel :]")
+      fig=px.pie(df8,names="Channel_Name",values="Total_videos")
+      fig.update_layout(
+        title="No.of videos published in 2022 " )
+      st.plotly_chart(fig)
 
 def Query9():
       query9="""SELECT Channel_Name,avg(TIME_TO_SEC(Duration)) AS Duration_in_seconds FROM video GROUP BY Channel_Name"""
@@ -294,6 +368,11 @@ def Query9():
       c9=cursor.column_names
       df9=pd.DataFrame(r9,columns=c9)
       st.write(df9)
+      st.write("### :green[Average video duration of each channel  :]")
+      fig = go.Figure(go.Pie(labels=df9['Channel_Name'], values=df9['Duration_in_seconds'], hole=0.5))
+      fig.update_layout(
+        title="Average video duration of each channel" )
+      st.plotly_chart(fig)
 
 def Query10():
       query10="""SELECT Channel_Name, Title AS Video_names ,Comments AS comment_count FROM video ORDER BY Comments DESC"""
@@ -303,10 +382,7 @@ def Query10():
       df10=pd.DataFrame(r10,columns=c10)
       st.write(df10)
 
-#streamlit tab
 tab1, tab2, tab3, tab4 = st.tabs(["$\large📝COLLECT DATA📝 $", "$\large🍃TRANSFER🍃 $", "$\large🐬MIGRATE🐬  $", "$\large📊VIEW📊 $" ])
-
-#tab collect data
 with tab1:
     st.header("Collection of data page")
     if st.button("Collect Channel Details"):
@@ -333,7 +409,6 @@ with tab1:
         st.write(' Extracted data from comments ')
         st.write(df_c)
 
-#tab transfer
 with tab2:
     st.header("Transfer datas to mongodb page")
     if st.button("Transfer Channel to mongodb"):
@@ -350,47 +425,49 @@ with tab2:
             st.success("Channel details uploaded to mongodb successfully!!!")
 
     if st.button("Transfer Videos to mongodb"):
-        if not is_transfer_videos_done():
-            tv=transfer_videos()
-            st.session_state.transfer_videos_done = True
-            st.success("Video details uploaded to mongodb successfully!!!")
+        db=mongo_url['YouTube_Data']
+        col=db['video']
+        ids_vid=col.distinct("Channel_Id")
+        if user_input in ids_vid:
+           st.error("Video details already exists")
         else:
-           st.error("Details already transformed!!!")
+            tv=transfer_videos()
+            st.success("Video details uploaded to mongodb successfully!!!")
        
         
     if st.button("Transfer Comments to mongodb"):
-        if not is_transfer_comments_done():
-            tco=transfer_comments()
-            st.session_state.transfer_comments_done = True
-            st.success("Comments detail uploaded to mongodb successfully!!!")
+        db=mongo_url['YouTube_Data']
+        col=db['comment']
+        com_d= col.distinct("Channel_Id")
+        if user_input in com_d:
+           st.error("comment details already exists")
         else:
-            st.error("Details already transformed!!!")
+            tco=transfer_comments()
+            st.success("Comment details uploaded to mongodb successfully!!!")
 
-#tab migrate
 with tab3:
    st.header("Migrate datas to MySQL page")
    if st.button("Migrate Channel"):
-      try:
-          migrate_channel()
-          st.success("Channel details uploaded to mySQL successfully!!!")
-      except:
-          st.error("Details already transformed!!!")
+          mc=migrate_channel(user_input)
+          if mc:
+            st.success("Channel details uploaded to mySQL successfully!!!")
+          else:
+           st.error("Details already transformed!!!")
 
    if st.button("Migrate Video"):
-      try:
-          migrate_video()
-          st.success("Video details uploaded to MySQL successfully!!!")
-      except:
-          st.error("Details already transformed!!!")
+          mv=migrate_video(user_input)
+          if mv:
+            st.success("Video details uploaded to MySQL successfully!!!")
+          else:
+           st.error("Details already transformed!!!")
 
    if st.button("Migrate comments"):
-      try:
-          migrate_comments()
-          st.success("Comments detail uploaded to MySQL successfully!!!")
-      except:
-          st.error("Details already transformed!!!")
+          mco=migrate_comments(user_input)
+          if mco:
+            st.success("Comments detail uploaded to MySQL successfully!!!")
+          else:
+           st.error("Details already transformed!!!")
 
-#tab view
 with tab4:    
    st.header("Queries page")
    st.write("Select Queries and view the result")
@@ -435,6 +512,7 @@ with tab4:
 
    if selection == "10. Which videos have the highest number of comments, and what are their corresponding channel names?":
        Query10()
+    
     
   
 
